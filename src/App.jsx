b00d7f965715cb8d2bb0f32c96ef1ad33dfb32b8
waynekiprotich <waynekip123@icloud.com>
2026-05-20 16:19:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,101 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
-import Login          from './pages/Login';
-import Signup         from './pages/Signup';
-import Dashboard      from './pages/Dashboard';
-import Challenges     from './pages/Challenges';
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Challenges from './pages/Challenges';
 import ChallengeDetail from './pages/ChallengeDetail';
-import Leaderboard    from './pages/Leaderboard';
-import Groups         from './pages/Groups';
-import Friends        from './pages/Friends';
-import Notifications  from './pages/Notifications';
-import Profile        from './pages/Profile';
-import NotFound       from './pages/NotFound';
+import Leaderboard from './pages/Leaderboard';
+import Groups from './pages/Groups';
+import Friends from './pages/Friends';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          {/* Public */}
-          <Route path="/login"  element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          {/* Protected */}
-          <Route path="/"               element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/challenges"     element={<ProtectedRoute><Challenges /></ProtectedRoute>} />
-          <Route path="/challenges/:id" element={<ProtectedRoute><ChallengeDetail /></ProtectedRoute>} />
-          <Route path="/leaderboard"    element={<ProtectedRoute><Leaderboard /></ProtectedRoute>} />
-          <Route path="/groups"         element={<ProtectedRoute><Groups /></ProtectedRoute>} />
-          <Route path="/friends"        element={<ProtectedRoute><Friends /></ProtectedRoute>} />
-          <Route path="/notifications"  element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
-          <Route path="/profile"        element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-          {/* 404 */}
+          <Route
+            path="/challenges"
+            element={
+              <ProtectedRoute>
+                <Challenges />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/challenges/:id"
+            element={
+              <ProtectedRoute>
+                <ChallengeDetail />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/leaderboard"
+            element={
+              <ProtectedRoute>
+                <Leaderboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/groups"
+            element={
+              <ProtectedRoute>
+                <Groups />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/friends"
+            element={
+              <ProtectedRoute>
+                <Friends />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/notifications"
+            element={
+              <ProtectedRoute>
+                <Notifications />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
